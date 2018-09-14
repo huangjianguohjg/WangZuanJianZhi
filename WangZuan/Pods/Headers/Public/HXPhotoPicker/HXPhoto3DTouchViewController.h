2a6427b79c0f7f82e62d6f6970f016b816d3dbc8
xiaohuanghuang <1490451648@qq.com>
2018-09-14 11:55:15 +0800

    fix
    fix

--- WangZuan/Pods/Headers/Public/HXPhotoPicker/HXPhoto3DTouchViewController.h
+++
@@ -1,1 +0,0 @@
-../../../HXPhotoPicker/照片选择器/HXPhotoPicker/HXPhoto3DTouchViewController.h
--- WangZuan/Pods/Target Support Files/Pods-WangZuan/Pods-WangZuan-resources.sh
+++ WangZuan/Pods/Target Support Files/Pods-WangZuan/Pods-WangZuan-resources.sh
@@ -93,14 +93,12 @@ EOM
 }
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_resource "${PODS_ROOT}/BRPickerView/BRPickerView/AddressPickerView/BRPickerView.bundle"
-  install_resource "${PODS_ROOT}/HXPhotoPicker/照片选择器/HXPhotoPicker/HXPhotoPicker.bundle"
   install_resource "${PODS_ROOT}/IQKeyboardManager/IQKeyboardManager/Resources/IQKeyboardManager.bundle"
   install_resource "${PODS_ROOT}/MJRefresh/MJRefresh/MJRefresh.bundle"
   install_resource "${PODS_ROOT}/SVProgressHUD/SVProgressHUD/SVProgressHUD.bundle"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_resource "${PODS_ROOT}/BRPickerView/BRPickerView/AddressPickerView/BRPickerView.bundle"
-  install_resource "${PODS_ROOT}/HXPhotoPicker/照片选择器/HXPhotoPicker/HXPhotoPicker.bundle"
   install_resource "${PODS_ROOT}/IQKeyboardManager/IQKeyboardManager/Resources/IQKeyboardManager.bundle"
   install_resource "${PODS_ROOT}/MJRefresh/MJRefresh/MJRefresh.bundle"
   install_resource "${PODS_ROOT}/SVProgressHUD/SVProgressHUD/SVProgressHUD.bundle"
